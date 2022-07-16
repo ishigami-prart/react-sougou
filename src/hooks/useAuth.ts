3e3e70e1a0231a5b93da359fd739c6ba840945b4
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-07-16 11:59:31 +0900

    login

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,9 +1,30 @@
 import axios from "axios";
 import { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+
+import { User } from "../api/user";
 
 export const useAuth = () => {
-  const login = useCallback((id: string) => {
-    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
-  }, []);
+  const history = useHistory();
+  const [loading, setLoading] = useState(false);
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            history.push("/home");
+          } else {
+            alert("ユーザーが見つかりません");
+          }
+        })
+        .catch(() => {
+          alert("ログインできません");
+        })
+        .finally(() => setLoading(false));
+    },
+    [history]
+  );
   return { login };
 };
