3207bbf12e7e2967312bfe365f6550609dfe1f7e
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-07-20 08:21:34 +0900

    画像

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,15 @@
-import { Box, Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Stack, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-        ></Box>
+        <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md">
+          <Stack textAlign="center">
+            <Image src="https://source.unsplash.com/random" />
+          </Stack>
+        </Box>
       </WrapItem>
     </Wrap>
   );
